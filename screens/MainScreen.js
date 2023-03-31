5b31d108e43972a49225c646b314e5c659d697b8
banip <rnaos0530@gmail.com>
2023-03-31 17:18:38 +0900

    깃헙링크 추가

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Container, Typography, Button } from './_atoms';
+import { FontAwesome5 } from '@expo/vector-icons';
+// 링크
+import { Linking } from 'react-native';
 
 function MainScreen({navigation}) {
     // next로 가는 링크 작성 
   return (
     <Container>
       <Typography style={style.title}>게임장르로 MBTI 검사</Typography>
-      <Button onPress={() => navigation.navigate('QuizScreen')}>
+      <Button onPress={() => navigation.navigate('QuizScreen')} style={{marginBottom: 20}}>
         <Typography>테스트 시작</Typography>
-
       </Button>
-
-      
+      <FontAwesome5 
+        name="github" size={30} color="#fff" onPress={() => Linking.openURL('https://github.com/byongshintv/mbti-check-by-game-genre')} />
     </Container>
   );
 }
